0069d511bbf349849a6e3123dd6d27d504032299
Kate Ponomareva <katya.ponomar006@mail.ru>
2024-10-11 15:39:51 +0300

    Initial commit

---
+++ hello.pas
@@ -0,0 +1,8 @@
+﻿##
+var Language := readstring('Выберете язык: ');
+case Language of
+  'Русский': print('Здравсвуйте!');
+  'Английский': print('Hello!');
+  'Турецкий': print('Merhaba!');
+  'Испанский': print('Hola!');
+end; 
